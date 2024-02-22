809c1c173bcc06f047ad9f756f14252371e4c37b
SudoLoonaFox <sudoloonafox@gmail.com>
2024-02-22 20:59:36 +1300

    Conway's game of life works

--- src/wireworld.c
+++ src/wireworld.c
@@ -38,6 +38,45 @@ void free_buffer(BUFFER* buffer){
 	return;
 }
 
+// TODO look for bug in this or next function
+int cell_state(BUFFER* buffer, int h, int w){
+	if(w<0 || w >= buffer->width){
+		return -1;
+	}
+	if(h<0 || h >= buffer->height){
+		return -1;
+	}
+	return buffer->buffer[buffer->width*h+w];
+}
+
+int moore_neighbours(BUFFER* buffer, int h, int w, int test_val){
+	int neighbours = 0;
+	if(cell_state(buffer, h+1, w-1)==test_val){
+		neighbours++;// NW
+	}
+	if(cell_state(buffer, h+1, w)==test_val){
+		neighbours++; // N
+	}
+	if(cell_state(buffer, h+1, w+1)==test_val){
+		neighbours++; // NE
+	}
+	if(cell_state(buffer, h, w-1)==test_val){
+		neighbours++; // W
+	}
+	if(cell_state(buffer, h, w+1)==test_val){
+		neighbours++; // E
+	}
+	if(cell_state(buffer, h-1, w-1)==test_val){
+		neighbours++; // SW
+	}
+	if(cell_state(buffer, h-1, w)==test_val){
+		neighbours++; // S
+	}
+	if(cell_state(buffer, h-1, w+1)==test_val){
+		neighbours++; // SE
+	}
+	return neighbours;
+}
 void draw_buffer(WINDOW* win, BUFFER* buffer, chtype* symbols){
 	wmove(win, 0,0);
 	for(int i = 0; i < buffer->height * buffer->width; i++){
@@ -46,9 +85,45 @@ void draw_buffer(WINDOW* win, BUFFER* buffer, chtype* symbols){
 	}
 }
 
+void conway_life_random(BUFFER* buffer){
+	for(int i = 0; i < buffer->width*buffer->height; i++){
+		buffer->buffer[i] = rand()%2;
+	}
+}
+
+void conway_life_tick(BUFFER* buffer, BUFFER* new_buffer){
+	if(buffer->height*buffer->width != new_buffer->height*new_buffer->width){
+		return;
+	}
+	int n;
+	for(int i = 0; i < buffer->height*buffer->width; i++){
+		n = moore_neighbours(buffer, i/buffer->width, i%buffer->width, 1);
+		if(n<2){
+			new_buffer->buffer[i] = 0;
+			continue;
+		}
+		if(n==3){
+			new_buffer->buffer[i] = 1;
+			continue;
+		}
+		if(n>3){
+			new_buffer->buffer[i] = 0;
+			continue;
+		}
+	}
+}
+
+void swap_buffers(BUFFER** buffer_1, BUFFER** buffer_2){
+	BUFFER* temp = *buffer_1;
+	*buffer_1 = *buffer_2;
+	*buffer_2 = temp;
+	return;
+}
+
 int main(){
 	// TODO add colors
 	setlocale(LC_ALL, "");
+	srand(time(NULL));
 	initscr(); // init screen
 	cbreak(); // disable character buffering
 	noecho(); // supress auto echo of typed characters
@@ -85,12 +160,18 @@ int main(){
 	BUFFER* front_buffer = create_buffer(height, width);
 	BUFFER* back_buffer = create_buffer(height, width);
 
-	for(int i = 0; i < height*width; i++){
-		front_buffer->buffer[i] = i%4;
-	}
-	draw_buffer(win, front_buffer, wireworld_symbols); 
-	wrefresh(win);
+	/*for(int i = 0; i < height*width; i++){
+		front_buffer->buffer[i] = i%2;
+	}*/
+	conway_life_random(front_buffer);
 
+	while(1){
+		conway_life_tick(front_buffer, back_buffer);
+		draw_buffer(win, front_buffer, wireworld_symbols); 
+		wrefresh(win);
+		swap_buffers(&front_buffer, &back_buffer);
+		delay(100);
+	}
 	while(1);
 	free_buffer(front_buffer);
 	free_buffer(back_buffer);
